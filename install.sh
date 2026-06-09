085f76a1d2cd15ccfb13e38c7287092ac444fb1b
manorossa078-dev <manorossa078@gmail.com>
2026-06-09 14:52:05 +0200

    Add files via upload

---
+++ install.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+readonly REQUIRED=("metaploit-framework")
+readonly RED="\e[1;31m"
+readonly GREEN="\e[1;32m"
+readonly YELLOW="\e[1;33m"
+readonly RESET="\e[0m"
+
+for tool in "${REQUIRED[@]}"; do
+	if [ $? -eq 0 ]; then
+		echo -e "${GREEN}[+] $tool already installed."
+	else
+		echo -e "${YELLOW}[*] ool isn't installed. Installing now...${RESET}"
+		sudo apt install $tool
+	fi
+done
+echo -e "${GREEN}[+] All tools are installed!"
