5e868ab4598369bd076603d44333a816060d0a7f
manorossa078-dev <manorossa078@gmail.com>
2026-06-13 17:40:39 +0200

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,17 +1,24 @@
 #!/bin/bash
 
-readonly REQUIRED=("metaploit-framework")
+readonly REQUIRED=("metasploit-framework")
 readonly RED="\e[1;31m"
 readonly GREEN="\e[1;32m"
 readonly YELLOW="\e[1;33m"
 readonly RESET="\e[0m"
 
 for tool in "${REQUIRED[@]}"; do
-	if [ $? -eq 0 ]; then
-		echo -e "${GREEN}[+] $tool already installed."
-	else
-		echo -e "${YELLOW}[*] $tool isn't installed. Installing now...${RESET}"
-		sudo apt install $tool
-	fi
+        dpkg -s "$tool" &> /dev/null
+        if [ $? -eq 0 ]; then
+                echo -e "${GREEN}[+] $tool is already installed."
+        else
+                echo -e "${YELLOW}[*] $tool isn't installed. Installing now...${RESET}"
+                yes | sudo apt install $tool
+                if [ $? -eq 0 ]; then
+                        echo -e "${GREEN}[+] Tool $tool succesfully installed!"
+                        clear
+                else
+                        echo -e "${RED}[!] Error on the installation of $tool."
+                fi
+        fi
 done
 echo -e "${GREEN}[+] All tools are installed!"
